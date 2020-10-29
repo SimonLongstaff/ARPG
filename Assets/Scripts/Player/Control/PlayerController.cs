943d043fd4eaa0ea0f7bf542c1fb9a6a32581815
Simon <simonlngstaff@gmail.com>
2020-10-29 16:26:21 +0000

    init - combat and movement basic

---
+++ Assets/Scripts/Player/Control/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Movement;
+using System;
+using RPG.Combat;
+
+namespace RPG.Controll
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MovementInteract();
+            CombatInteract();
+        }
+
+        private void CombatInteract()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) { continue; }
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void MovementInteract()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
+
+            bool hasHit = Physics.Raycast(GetMouseRay(), out RaycastHit hit);
+
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Mover.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            updateAnimation();
+        }
+
+        private void updateAnimation()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("Blend", speed);
+
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+    }
+}
